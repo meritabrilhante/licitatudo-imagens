09fddaa6f8756f0e249fe6f1aac6415a8a4288ad
Rita Brilhante <meritabrilhante@gmail.com>
2023-10-03 16:38:16 -0300

    Add files via upload

---
+++ images/partners/index.ts
@@ -0,0 +1,4 @@
+import saboretti from 'public/assets/images/partners/saboretti.png'
+import italinea from 'public/assets/images/partners/italinea.png'
+import todeschini from 'public/assets/images/partners/todeschini.png'
+import laPriori from 'public/assets/images/partners/la-priori.png'
